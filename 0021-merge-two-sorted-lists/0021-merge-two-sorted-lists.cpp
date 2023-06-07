d47f1fb391893465eea30c8bfe2afc7c3367ba14
Shrinivas Hatyalikar <104452260+Shriii04@users.noreply.github.com>
2023-06-07 19:25:19 +0530

    Time: 8 ms (70.21%), Space: 14.7 MB (97.86%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode *p,*q;
+    if(list1==NULL && list2==NULL){
+        return NULL;
+    }
+    if(list1==NULL){
+        return list2;
+    }
+    if(list2==NULL){
+        return list1;
+    }
+    if(list1->val < list2->val){
+        p=list1;
+        q=list1;
+        list1=list1->next;
+        q->next=NULL;
+    }
+    else{
+        p=list2;
+        q=list2;
+        list2=list2->next;
+        q->next=NULL;
+    }
+    while(list1!=NULL&&list2!=NULL)
+    {
+        if(list1->val<list2->val)
+        {
+        q->next=list1;
+        q=list1;
+        list1=list1->next;
+        q->next=NULL;
+        }
+        else
+        {
+            q->next=list2;
+            q=list2;
+            list2=list2->next;
+            q->next=NULL;
+        }
+    }
+    while(list1!=NULL)
+    {
+        q->next=list1;
+        q=list1;
+        list1=list1->next;
+        q->next=NULL;
+    }
+    while(list2!=NULL)
+    {
+        q->next=list2;
+        q=list2;
+        list2=list2->next;
+        q->next=NULL;
+    }
+    return p;
+}
+
+};
