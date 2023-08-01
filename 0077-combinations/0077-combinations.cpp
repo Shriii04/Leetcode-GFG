9f2f06f64fbf7886a0c643031aed5faf9b26caba
Shrinivas Hatyalikar <104452260+Shriii04@users.noreply.github.com>
2023-08-01 12:43:57 +0530

    Time: 14 ms (90.01%), Space: 9 MB (83.81%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    void solve(int i,int n,int k,vector<int>&ds,vector<vector<int>>&ans){
+            if(ds.size()==k){
+                ans.push_back(ds);
+                return;
+            }
+//         ds.push_back(i);
+//         solve(i+1,n,k,ds,ans);
+//         ds.pop_back();
+//         solve(i+1,n,k,ds,ans);
+        
+         for (int j = i; j <= n; j++) {
+            ds.push_back(j);
+            solve(j+1, n, k, ds, ans);
+            ds.pop_back();
+        }
+        
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(1,n,k,ds,ans);
+        return ans;
+        
+    }
+};
+
