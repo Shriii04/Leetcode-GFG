c9ee975094d6fcba5d3ea498583974b2fcf4b0ce
Shrinivas Hatyalikar <104452260+Shriii04@users.noreply.github.com>
2023-10-04 20:29:56 +0530

    Time: 14 ms (63.15%), Space: 11.2 MB (24.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.length()==0)return 0;   //if string of length zero comes simply return 0
-        unordered_map<char,int> m;   //create map to store frequency,(get to know all unique characters
-        int i=0,j=0,ans=INT_MIN; 
-        while(j<s.length())   
-        {
-            m[s[j]]++;  //increase the frequency of the element as you traverse the string
-            if(m.size()==j-i+1)  // whem map size is equal to the window size means suppose window size is 3 and map size is also three that means in map all unique characters are their
-            {
-                ans = max(ans,j-i+1);  //compare the length of the maximum window size
-            }
-            else if(m.size()<j-i+1)   //if the map size is less than the window size means there is some duplicate present like window size = 3 and map size = 2 means there is a duplicates
-            {
-                while(m.size()<j-i+1)  //so till the duplicates are removed completely
-                {
-                    m[s[i]]--;   //remove the duplicates
-                    if(m[s[i]]==0)  //if the frequency becomes zero 
-                    {
-                        m.erase(s[i]);//delete it completely
-                    }
-                    i++;  //go for next element 
+        if(s.size()==0){
+            return 0;
+        }
+        int ans = INT_MIN;
+        unordered_set<int> s1;
+        int l=0;
+        for(int r=0;r<s.size();r++){
+            if(s1.find(s[r])!=s1.end()){
+                while(l<r && s1.find(s[r])!=s1.end() ){
+                    s1.erase(s[l]);
+                    l++;
                 }
             }
-             j++;  //go for the next element
+            s1.insert(s[r]);
+            ans = max(ans,r-l+1);
         }
         return ans;
     }
